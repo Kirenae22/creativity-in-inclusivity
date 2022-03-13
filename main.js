ad87ba67ac491e3603d01ea660cd7b2d9dbf6d3a
Kirenae22 <99567445+Kirenae22@users.noreply.github.com>
2022-03-13 19:11:32 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function toggleDarkMode() {
     let background = document.getElementById ("backgroundimage");
      if(background.style.background === "black"){
-         background.style.background = "white";
-     } else {
+         background.style.background = "white"; 
+    } else {
          background.style.background = "black";
      }
  }
